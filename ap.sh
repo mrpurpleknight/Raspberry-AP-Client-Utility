d66bf09e46caf0e44c136268cf21818f52d39aba
mrpurpleknight <gabriele.schincaglia@student.supsi.ch>
2021-04-21 18:55:46 +0200

    fixed appending file like a fool

--- ap.sh
+++ ap.sh
@@ -18,7 +18,6 @@ function up() {
         country_code="$line"
     done < .config.ap
 
-    clearApFiles
     writeApFiles "$country_code" "$ssid"
 
     systemctl daemon-reload
@@ -45,9 +44,9 @@ function writeApFiles() {
 
     #dhcpcd, the DHCP client on the Raspberry Pi, automatically requests an IP address for every active interface.
     #So we need to block the eth0 and wlan0 interfaces from being processed, and let dhcpcd configure only br0 via DHCP
-    printf "denyinterfaces wlan0 eth0\ninterface br0" >>/etc/dhcpcd.conf
+    printf "denyinterfaces wlan0 eth0\ninterface br0" >/etc/dhcpcd.conf
 
-    printf "country_code=$country_code\ninterface=wlan0\nbridge=br0\nssid=$ssid\nhw_mode=g\nchannel=7\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_passphrase=AccedyBox!\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n" >>/etc/hostapd/hostapd.conf
+    printf "country_code=$country_code\ninterface=wlan0\nbridge=br0\nssid=$ssid\nhw_mode=g\nchannel=7\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_passphrase=AccedyBox!\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n" >/etc/hostapd/hostapd.conf
 }
 
 function setupAp() {
@@ -64,10 +63,10 @@ function setupAp() {
     rfkill unblock wlan
     
     #Add a bridge network device named br0
-    printf "[NetDev]\nName=br0\nKind=bridge" >>/etc/systemd/network/bridge-br0.netdev
+    printf "[NetDev]\nName=br0\nKind=bridge" >/etc/systemd/network/bridge-br0.netdev
 
     #In order to bridge the Ethernet network with the wireless network, first add the built-in Ethernet interface (eth0) as a bridge member
-    printf "[Match]\nName=eth0\n\n[Network]\nBridge=br0" >>/etc/systemd/network/br0-member-eth0.network
+    printf "[Match]\nName=eth0\n\n[Network]\nBridge=br0" >/etc/systemd/network/br0-member-eth0.network
 
     writeApFiles "$country_code" "FirstSetup"
 
