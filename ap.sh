e2ed87b5077e6ab6dcafb7edab4b573ddae0894c
mrpurpleknight <gabriele.schincaglia@student.supsi.ch>
2021-04-22 22:59:56 +0200

    rwx on every conf file

--- ap.sh
+++ ap.sh
@@ -14,6 +14,7 @@ function down() {
     systemctl disable systemd-networkd
     systemctl stop systemd-networkd.socket 
     systemctl stop systemd-networkd
+
     systemctl reset-failed
     
     systemctl restart systemd-resolved
@@ -54,7 +55,7 @@ function clearApFiles() {
 
     rm -rf /etc/dhcpcd.conf
     touch /etc/dhcpcd.conf
-    chmod +x /etc/dhcpcd.conf
+    chmod +rwx /etc/dhcpcd.conf
 
 }
 
@@ -64,14 +65,16 @@ function writeApFiles() {
 
     #Add a bridge network device named br0
     printf "[NetDev]\nName=br0\nKind=bridge" >/etc/systemd/network/bridge-br0.netdev
+    chmod +rwx /etc/systemd/network/bridge-br0.netdev
 
     #In order to bridge the Ethernet network with the wireless network, first add the built-in Ethernet interface (eth0) as a bridge member
     printf "[Match]\nName=eth0\n\n[Network]\nBridge=br0" >/etc/systemd/network/br0-member-eth0.network
+    chmod +rwx /etc/systemd/network/br0-member-eth0.network
 
     #dhcpcd, the DHCP client on the Raspberry Pi, automatically requests an IP address for every active interface.
     #So we need to block the eth0 and wlan0 interfaces from being processed, and let dhcpcd configure only br0 via DHCP
     printf "denyinterfaces wlan0 eth0\ninterface br0" >/etc/dhcpcd.conf
-    chmod +x /etc/dhcpcd.conf
+    chmod +rwx /etc/dhcpcd.conf
 
     printf "country_code=$country_code\ninterface=wlan0\nbridge=br0\nssid=$ssid\nhw_mode=g\nchannel=7\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_passphrase=AccedyBox!\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP\n" >/etc/hostapd/hostapd.conf
 }
